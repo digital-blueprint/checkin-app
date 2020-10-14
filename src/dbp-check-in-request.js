d0ac4b951c5d5441560ed8c57dae2585e246601a
Tamara Steinwender <tamara.steinwender@tugraz.at>
2020-10-14 13:11:17 +0200

    Add number check to seat number, Update submodule

--- src/dbp-check-in-request.js
+++ src/dbp-check-in-request.js
@@ -421,6 +421,7 @@ class CheckIn extends ScopedElementsMixin(DBPCheckInLitElement) {
     setSeatNumber(e) {
         let val = parseInt( e.explicitOriginalTarget.value );
         val = isNaN(val) ? "" : val;
+        val = val > this.roomCapacity ? this.roomCapacity : val;
         this._("#select-seat").value = val;
     }
 
@@ -561,6 +562,9 @@ class CheckIn extends ScopedElementsMixin(DBPCheckInLitElement) {
                     display: block;
                     width: 100%;
                 }
+                #select-seat{
+                    width: 100%;
+                }
             }
         `;
     }
@@ -615,7 +619,7 @@ class CheckIn extends ScopedElementsMixin(DBPCheckInLitElement) {
                             <link rel="stylesheet" href="${select2CSS}">
                             <label class="label">${i18n.t('check-in.manually-seat')}</label>
                             <div class="control">
-                                <input type="text" id="select-seat" name="seat-number" min="1" max="${this.roomCapacity}" placeholder="0" maxlength="4" inputmode="numeric" pattern="[0-9]*" ?disabled=${!this.isRoomSelected} @input="${(event) => {this.setSeatNumber(event);}}"> <!-- //TODO Styling of arrows -->
+                                <input class="input" type="text" id="select-seat" name="seat-number" min="1" max="${this.roomCapacity}" placeholder="1-${this.roomCapacity}" maxlength="4" inputmode="numeric" pattern="[0-9]*" ?disabled=${!this.isRoomSelected} @input="${(event) => {this.setSeatNumber(event);}}"> <!-- //TODO Styling of arrows -->
                             </div>
                         </div>
                     </form>
