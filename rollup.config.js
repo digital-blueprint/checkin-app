a1253dd9931327829f0edbae2a3f3f7b1a9967b3
Christoph Reiter <reiter.christoph@gmail.com>
2020-10-22 14:13:25 +0200

    Add 0BSD to the allowed license list

--- rollup.config.js
+++ rollup.config.js
@@ -221,7 +221,7 @@ Dependencies:
 `},
           thirdParty: {
             allow: {
-              test: '(MIT OR BSD-3-Clause OR Apache-2.0 OR LGPL-2.1-or-later)',
+              test: '(MIT OR BSD-3-Clause OR Apache-2.0 OR LGPL-2.1-or-later OR 0BSD)',
               failOnUnlicensed: true,
               failOnViolation: true,
             },
