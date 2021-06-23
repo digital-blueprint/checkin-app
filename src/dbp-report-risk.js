07f4a458a594d8620834af3ead26b58173f8d29f
Patrizio Bekerle <patrizio@bekerle.com>
2021-06-23 11:03:14 +0200

    Move dbp-report-risk TU Graz specifics to slot and fill it in the root html

--- src/dbp-report-risk.js
+++ src/dbp-report-risk.js
@@ -60,7 +60,7 @@ class ReportRisk extends ScopedElementsMixin(DBPCheckInLitElement) {
                 margin-bottom: 10px;
                 margin-top: 0px;
             }
-        
+
             .border {
                 margin-top: 2rem;
                 border-top: 1px solid black;
@@ -69,56 +69,12 @@ class ReportRisk extends ScopedElementsMixin(DBPCheckInLitElement) {
             .container {
                 margin-top: 2rem;
             }
-            
+
             .loading {
                 text-align: center;
                 display: flex;
                 justify-content: center;
                 padding: 30px;
-            }
-
-            .int-link-external{
-                transition: background-color 0.15s, color 0.15s;
-                border-bottom: 1px solid rgba(0,0,0,0.3);
-            }
-            
-            .int-link-external:hover{
-                background-color: black;
-                color: white;
-            }
-            
-            .int-link-external:after{
-                content: "\\00a0\\00a0\\00a0";
-                background-image: url('data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3Ardf%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20height%3D%228.6836mm%22%20width%3D%225.2043mm%22%20version%3D%221.1%22%20xmlns%3Acc%3D%22http%3A%2F%2Fcreativecommons.org%2Fns%23%22%20xmlns%3Adc%3D%22http%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%22%20viewBox%3D%220%200%2018.440707%2030.768605%22%3E%3Cg%20transform%3D%22translate(-382.21%20-336.98)%22%3E%3Cpath%20style%3D%22stroke-linejoin%3Around%3Bstroke%3A%23000%3Bstroke-linecap%3Around%3Bstroke-miterlimit%3A10%3Bstroke-width%3A2%3Bfill%3Anone%22%20d%3D%22m383.22%20366.74%2016.43-14.38-16.43-14.37%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E');
-                background-size: 73%;
-                background-repeat: no-repeat;
-                background-position: center center;
-                margin: 0 0 0 3px;
-                padding: 0 0 0.25% 0;
-                animation: 0.15s linkIconOut;
-                font-size: 103%;
-            }
-            
-            .int-link-external:hover::after{
-                content: "\\00a0\\00a0\\00a0";
-                background-image: url('data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3Ardf%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20height%3D%228.6836mm%22%20width%3D%225.2043mm%22%20version%3D%221.1%22%20xmlns%3Acc%3D%22http%3A%2F%2Fcreativecommons.org%2Fns%23%22%20xmlns%3Adc%3D%22http%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%22%20viewBox%3D%220%200%2018.440707%2030.768605%22%3E%3Cg%20transform%3D%22translate(-382.21%20-336.98)%22%3E%3Cpath%20style%3D%22stroke-linejoin%3Around%3Bstroke%3A%23FFF%3Bstroke-linecap%3Around%3Bstroke-miterlimit%3A10%3Bstroke-width%3A2%3Bfill%3Anone%22%20d%3D%22m383.22%20366.74%2016.43-14.38-16.43-14.37%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E');
-                background-size: 73%;
-                background-repeat: no-repeat;
-                background-position: center center;
-                margin: 0 0 0 3px;
-                padding: 0 0 0.25% 0;
-                animation: 0s linkIconIn;
-                font-size: 103%;
-            }
-
-            .int-email-external{
-                transition: background-color 0.15s, color 0.15s;
-                border-bottom: 1px solid rgba(0,0,0,0.3);
-            }
-            
-            .int-email-external:hover{
-                background-color: black;
-                color: white;
             }
 
             @media only screen
@@ -139,25 +95,20 @@ class ReportRisk extends ScopedElementsMixin(DBPCheckInLitElement) {
                     <dbp-mini-spinner text=${i18n.t('check-out.loading-message')}></dbp-mini-spinner>
                 </span>
             </div>
-        
+
             <div class="notification is-warning ${classMap({hidden: this.isLoggedIn() || this.isLoading()})}">
                     ${i18n.t('error-login-message')}
             </div>
-    
+
             <div class="${classMap({hidden: !this.isLoggedIn() || this.isLoading()})}">
-    
                 <vpu-notification lang="de" client-id="my-client-id"></vpu-notification>
                 <h2>${i18n.t('report-risk.title')}</h2>
                 <p class="">${i18n.t('report-risk.description')}</p>
-                <p>
-                    ${i18n.t('report-risk.text')}
-                    <a target="_blank" rel="noopener" href="mailto:coronameldung@tugraz.at" class="int-email-external"><span>coronameldung@tugraz.at</span></a>!
-                </p>
-                <p>
-                    ${i18n.t('report-risk.more-information')}
-                    <a target="_blank" rel="noopener" href="https://www.tugraz.at/go/coronavirus" target="_blank" class="int-link-external"><span>www.tugraz.at/go/coronavirus</span></a>.
-                </p>
-            
+                <slot name="extra-description">
+                    <p>
+                        ${i18n.t('report-risk.text')}
+                    </p>
+                </slot>
             </div>
         `;
     }
