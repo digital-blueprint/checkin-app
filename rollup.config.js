2936a80b246fae07c5f1b34d08105a65aacd778f
Christoph Reiter <reiter.christoph@gmail.com>
2023-10-30 15:14:32 +0100

    rollup: fix watch-custom command
    * Fix wrong custom assets path
    * Get rid of template vars that are never set

--- rollup.config.js
+++ rollup.config.js
@@ -33,7 +33,7 @@ let whitelabel;
 // path to non whitelabel assets and configs
 let customAssetsPath;
 // development path
-let devPath = 'assets_custom/checkin-app/assets/';
+let devPath = 'assets_custom/';
 // deployment path
 let deploymentPath = '../assets/';
 
