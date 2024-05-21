cb7ebd91992ee531889d738c913eb46681b3fe85
Christoph Reiter <reiter.christoph@gmail.com>
2024-05-21 14:37:06 +0200

    rollup: remove unsafe-eval from the CSP
    this was required for keycloak, but that's no longer the case

--- rollup.config.js
+++ rollup.config.js
@@ -105,7 +105,7 @@ function getOrigin(url) {
     return '';
 }
 
-config.CSP = `default-src 'self' 'unsafe-eval' 'unsafe-inline' \
+config.CSP = `default-src 'self' 'unsafe-inline' \
     ${getOrigin(config.matomoUrl)} ${getOrigin(config.keyCloakBaseURL)} \
     ${getOrigin(config.entryPointURL)} \
     ${getOrigin(config.nextcloudBaseURL)}; \
